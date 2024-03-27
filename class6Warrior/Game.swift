0adb9208387695a20d011f48d065540d77be5664
Nurdaulet <ndauletkhan2004@mail.ru>
2024-03-27 15:53:13 +0500

    first commit

---
+++ class6Warrior/Game.swift
@@ -0,0 +1,12 @@
+//
+//  Game.swift
+//  class6Warrior
+//
+//  Created by Нурдаулет Даулетхан on 11.03.2024.
+//
+
+import Foundation
+
+class Game{
+    
+}
---
+++ class6Warrior/warriorInfo.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  class6Warrior
+//
+//  Created by Нурдаулет Даулетхан on 11.03.2024.
+//
+
+import Foundation
+
+struct Character {
+    var name: String
+    var health: Int
+    var damage: Int
+}
+
